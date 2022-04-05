8321e686d3101e81bd493a77f5a606cf30c0cf84
lahenisarga15 <103022947+lahenisarga15@users.noreply.github.com>
2022-04-05 12:09:33 +0530

    Add files via upload

---
+++ calculation.py
@@ -0,0 +1,32 @@
+#this is python programme to perform addition
+#programme performed by nisarga lahe
+
+num1=20
+num2=10
+add=num1+num2
+print(add)
+
+#this is the programme to perform subtraction
+#programme performed by nisarga lahe
+
+a=25
+b=15
+subtract=a-b
+print(subtract)
+
+
+#this programme is to perform multiplication
+#programme performed by nisarga lahe
+
+x=7
+y=8
+multiply=x*y
+print(multiply)
+
+#this programme is performed for division
+#programme performed by nisarga lahe
+
+num1=10
+num2=5
+divide=num1/num2
+print(divide)
