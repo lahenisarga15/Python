1a8e8527619c74ccbe1930538f1811a37621eebb
lahenisarga15 <103022947+lahenisarga15@users.noreply.github.com>
2022-04-12 12:01:51 +0530

    Add files via upload

---
+++ pattern1.py
@@ -0,0 +1,13 @@
+#wpp to print square pattern
+row=0
+column=0
+for row in range(4):
+    print()
+    for column in range(4):
+        print('*',end=" ")
+
+#wpp to print digonal matrix or right angle traingle
+
+row=0
+
+        
---
+++ pattern2.py
@@ -0,0 +1,8 @@
+#wpp to print digonal matrix or right angle traingle
+
+row=0
+column=0
+for row in range(4):
+    print()
+    for column in range(row+1):
+        print('*',end=" ")
