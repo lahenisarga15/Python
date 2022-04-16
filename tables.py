ac347eed556114a8471e12b2bbc9c6f2c5d9820b
lahenisarga15 <103022947+lahenisarga15@users.noreply.github.com>
2022-04-16 12:04:46 +0530

    Add files via upload

---
+++ tables.py
@@ -0,0 +1,16 @@
+#python progrmme to print multiplication table
+#programme performed by nisarga lahe
+
+n=int(input("enter the no: "))
+#for i in range (1,11):
+# print(n*i)
+
+# function to print multiplication table
+def table(num):
+    for  i in range (1,11):
+        print(n*i)
+
+table(n) 
+
+      
+        
