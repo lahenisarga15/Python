1f15b1c00213d9a9e84e955815d560af99305b2e
lahenisarga15 <103022947+lahenisarga15@users.noreply.github.com>
2022-04-16 12:04:19 +0530

    Add files via upload

---
+++ simple calculator.py
@@ -0,0 +1,29 @@
+#python programme to make simple calculator
+#programme performed by nisarga lahe
+
+
+#function to add two numbers
+def add(a,b):
+    print(a+b)
+add(6,7)
+
+
+#function to subtract two numbers
+def subtract(x,y):
+    print(x-y)
+subtract(15,10)
+
+
+
+#function to multiply two numbrs
+def multiply(a,b):
+    print(a*b)
+multiply(12,6)
+
+
+
+#function to divide two numbers
+def divide(x,y):
+    print(x/y)
+divide(45,9)
+
