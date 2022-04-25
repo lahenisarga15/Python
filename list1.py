33125579699b968c9cc20ea379556f1db6c9ead1
lahenisarga15 <103022947+lahenisarga15@users.noreply.github.com>
2022-04-25 11:30:14 +0530

    Add files via upload

---
+++ list1.py
@@ -0,0 +1,28 @@
+#Python programme to perform appendind one list to another
+#Programme performed by nisarga lahe.
+
+list1 =[1,2,3,4,5]
+list2=["voilet","indigo","blue","green","yellow","orange","red"]
+
+final_list=list1+list2
+print(final_list)
+
+#Python programme for appending a list.
+#Programme performed by nisarga lahe
+
+list1=["bag","clothes","shoes","mobile","phone","laptop"]
+list1.append(["charger","tiffin","notebook"])
+print(list1)
+
+
+#Python programme for inserting in the list.
+list1=["mobile","clothes","shoes","laptop"]
+list1.insert(0,"laptop")
+print(list1)
+
+
+
+#Python programme for deleting from the list.
+list1=["tomato","potato","spinach","carrot","milk","curd","sugar","edible oil"]
+print(list1.pop(7))
+print(list1)
