20b36292b8fcbc7502a3e3b60c6c9a9ef10d613c
LoungeClown <70144726+loungeclown@users.noreply.github.com>
2022-04-27 17:11:13 +0530

    Grammatical errors fixed

--- pattern4.py
+++ pattern4.py
@@ -1,5 +1,9 @@
-#python programme to draw inverted right angled triangle
-#programme performed by nisarga lahe
+#python program to draw inverted right angled triangle
+#program written by nisarga lahe
+
+#programme is a word used for occasions and program is the thing you want to mention here :D and also performed seems kinda akward to read as it means you apparently danced or sang the program! haha
+#so well yea
+#btw delete the things i wrote here :D
 
 n=int(input("enter the no of rows:"))
 fot i in range 
