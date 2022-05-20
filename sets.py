6afcb827859143845c8494da7b93bd256fb57790
lahenisarga15 <103022947+lahenisarga15@users.noreply.github.com>
2022-05-20 10:58:45 +0530

    Add files via upload

---
+++ sets.py
@@ -0,0 +1,101 @@
+Python 3.6.8 (tags/v3.6.8:3c6b436a57, Dec 23 2018, 23:31:17) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+=============== RESTART: C:\Users\nisarga lahe\Desktop\sets.py ===============
+{'Monday', 'Sunday', 'Tuesday', 'Thursday', 'Friday', 'Wednesday', 'Saturday'}
+
+ <class 'set'>
+
+looping through thrrough the set elements...
+Monday
+Sunday
+Tuesday
+Thursday
+Friday
+Wednesday
+Saturday
+>>> 
+=============== RESTART: C:\Users\nisarga lahe\Desktop\sets.py ===============
+{'Tuesday', 'Thursday', 'Sunday', 'Saturday', 'Monday', 'Friday', 'Wednesday'}
+
+ <class 'set'>
+
+looping through thrrough the set elements...
+Tuesday
+Thursday
+Sunday
+Saturday
+Monday
+Friday
+Wednesday
+{'C', 'E', 'B', 'D', 'A', 'F'}
+>>> 
+=============== RESTART: C:\Users\nisarga lahe\Desktop\sets.py ===============
+{'Monday', 'Sunday', 'Thursday', 'Tuesday', 'Saturday', 'Friday', 'Wednesday'}
+
+ <class 'set'>
+
+looping through  the set elements...
+Monday
+Sunday
+Thursday
+Tuesday
+Saturday
+Friday
+Wednesday
+{'E', 'B', 'A', 'D', 'C', 'F'}
+>>> 
+=============== RESTART: C:\Users\nisarga lahe\Desktop\sets.py ===============
+{'Thursday', 'Monday', 'Tuesday', 'Wednesday', 'Saturday', 'Sunday', 'Friday'}
+
+ <class 'set'>
+
+looping through  the set elements...
+Thursday
+Monday
+Tuesday
+Wednesday
+Saturday
+Sunday
+Friday
+{'E', 'D', 'F', 'B', 'C', 'A'}
+{'Om'}
+>>> 
+>>> 
+=============== RESTART: C:\Users\nisarga lahe\Desktop\sets.py ===============
+{'Monday', 'Tuesday', 'Saturday', 'Sunday', 'Wednesday', 'Thursday', 'Friday'}
+
+ <class 'set'>
+
+looping through  the set elements...
+Monday
+Tuesday
+Saturday
+Sunday
+Wednesday
+Thursday
+Friday
+{'D', 'B', 'F', 'E', 'C', 'A'}
+{'Om'}
+{1, 5, 6}
+>>> 
+>>> 
+=============== RESTART: C:\Users\nisarga lahe\Desktop\sets.py ===============
+{'Saturday', 'Thursday', 'Friday', 'Tuesday', 'Sunday', 'Monday', 'Wednesday'}
+
+ <class 'set'>
+
+looping through  the set elements...
+Saturday
+Thursday
+Friday
+Tuesday
+Sunday
+Monday
+Wednesday
+{'B', 'E', 'C', 'A', 'D', 'F'}
+{'Om'}
+{1, 5, 6}
+True
+False
+>>> 
